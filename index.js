381a84ff8713ff8f05c405158bea89763e0265f2
JULIOPASCHOAL <juliocpaschoal@gmail.com>
2020-10-28 21:35:05 -0300

    Passando parametros obrigatório e opcional pela rota

--- index.js
+++ index.js
@@ -3,23 +3,38 @@ const app = express(); //Inicianod o express
 
 
 //Criando rotas 
-app.get("/", function(req,res){
+app.get("/", function (req, res) {
     res.send("Bem ao meus site!")
 });
 
-app.get("/blog", function(req,res){
-    res.send("Bem vindo ao meu blog");
+app.get("/blog/:artigo?", function (req, res) {
+
+    var artigo = req.params.artigo;
+    if (artigo) {
+        res.send("<h2>Artigo: " + artigo + "</h2>");
+    } else {
+        res.send("Bem vindo ao meu blog");
+    }
+
+
 });
 
-app.get("/canal/youtube", function(req,res){
+app.get("/canal/youtube", function (req, res) {
     res.send("<h1>Bem ao meu canal no youtube</h1>");
 });
+app.get("/ola/:nome/:empresa", function (req, res) {
+    // REQ => DADOS ENCIADOS PELO USUARIO
+    // RES => RESPOSTA QUE VAI SER ENVIADA AO USUARIO
+    var nome = req.params.nome;
+    var empresa = req.params.empresa;
+    res.send("<h1>Oi " + nome + " do " + empresa + " </h1>");
+});
 
 
-app.listen(4000,function(erro){
-    if(erro){
+app.listen(4000, function (erro) {
+    if (erro) {
         console.log("Ocorreu um erro!");
-    }else{
+    } else {
         console.log("Servidor iniciado com sucesso!");
     }
 });
